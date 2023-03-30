99efe61a8dd6c1753ee51e56804eeea820c83b73
Daniël van Minnen <daielino130904@gmail.com>
2023-03-30 08:54:29 +0200

    speciaal pagina

--- site/speciaal.php
+++ site/speciaal.php
@@ -18,22 +18,11 @@ $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/footer.css">
 </head>
 
 <body>
-    <?Php include 'nav.php'; ?>
 
-    <div class="foto1">
-        <img src=" <?php echo $recept["foto"] ?>">
-    </div>  
-    <div class="naam">
-        <h3><?php echo $recept["naam_gerecht"] ?></h3>
-    </div>
 
-    <?php include 'footer.php'; ?>
 </body>
 
 </html>
